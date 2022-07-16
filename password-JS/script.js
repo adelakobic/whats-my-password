869b7831856e4d5a9c4568988061cb906aae694c
adelakobic <alojzamauder@yahoo.co.uk>
2022-07-16 11:00:15 +0100

    laid out sections for JavaScript in comments

--- password-JS/script.js
+++ password-JS/script.js
@@ -1,7 +1,27 @@
 // Assignment Code
+// generateBtn links to HTML generate button line 31 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//window prompt opening message
+
+var userPrompt = window.prompt("Would you like to generate a new passowrd?", "yes or no");
+
+function generatePassword(){
+  console.log("Welcome to 'What's my password?'");
+
+  // 1 prompt user for password criteria
+  //     a password length between 8 and 128 characters
+  //     b uppercase, lowercase, numbers, special characters
+  // 2 validate the input (from 1)
+  // 3 genertae the actual password
+
+
+  // 4 display the generated password on the page 
+
+  return "Generated password will appear here!";
+}
+
+// Write password to the #password input (links to line 26 in HTML)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,4 +31,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// Links to generate password function
 generateBtn.addEventListener("click", writePassword);
